9bc56597937eb4cb508fdcdea6a4162cd34dbe53
kevinweber <k-iwi@web.de>
2014-12-01 21:08:48 +0100

    Fixed '<?='

--- admin/class-admin-options.php
+++ admin/class-admin-options.php
@@ -344,7 +344,7 @@ class Lazyload_Videos_Admin {
 			</form>
 
 	 		<div class="update-posts notice clear-both">
-				<form action="options-general.php?page=<?= LL_ADMIN_URL; ?>" method="post">
+				<form action="options-general.php?page=<?php echo LL_ADMIN_URL; ?>" method="post">
 				   <input type="hidden" name="update_posts" value="with_oembed" />
 				   <input class="button update-posts" type="submit" value="Update Posts" />
 				</form>
