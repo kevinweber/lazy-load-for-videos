a0ecee38512f2a39469b7f298ce8b767a4e2c559
kevinweber <k-iwi@web.de>
2015-01-05 19:00:40 +0100

    Changed Gravatar URL to https://...

--- admin/class-admin-options.php
+++ admin/class-admin-options.php
@@ -364,7 +364,7 @@ class Lazyload_Videos_Admin {
 
 		    <table class="form-table">
 		        <tr valign="top">
-			        <th scope="row" style="width:100px;"><a href="http://kevinw.de/ll" target="_blank"><img src="http://www.gravatar.com/avatar/9d876cfd1fed468f71c84d26ca0e9e33?d=http%3A%2F%2F1.gravatar.com%2Favatar%2Fad516503a11cd5ca435acc9bb6523536&s=100" style="-webkit-border-radius:50%;-moz-border-radius:50%;border-radius:50%;"></a></th>
+			        <th scope="row" style="width:100px;"><a href="http://kevinw.de/ll" target="_blank"><img src="https://www.gravatar.com/avatar/9d876cfd1fed468f71c84d26ca0e9e33?d=http%3A%2F%2F1.gravatar.com%2Favatar%2Fad516503a11cd5ca435acc9bb6523536&s=100" style="-webkit-border-radius:50%;-moz-border-radius:50%;border-radius:50%;"></a></th>
 			        <td style="width:200px;">
 			        	<p><a href="http://kevinw.de/ll" target="_blank">Kevin Weber</a> &ndash; that's me.<br>
 			        	I'm the developer of this plugin. I hope you enjoy it!</p>
