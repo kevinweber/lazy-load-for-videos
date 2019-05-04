a539ba76abe00da40870cb4f7ef049ccd90d69bd
Kevin Weber <k-iwi@web.de>
2019-05-04 11:53:43 -0700

    Add query util

---
+++ modules/utils/queryHashToString.test.js
@@ -0,0 +1,12 @@
+import queryHashToString from './queryHashToString'
+
+describe('queryHashToString', () => {
+  it('converts object into string', () => {
+    const queryObject = {
+      autoplay: 1,
+      'something-else': 'it totally works',
+    };
+
+    expect(queryHashToString(queryObject)).toBe('autoplay=1&something-else=it totally works')
+  });
+});
