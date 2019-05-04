579af023208efc54b0de67a0513420a854baebbf
Kevin Weber <k-iwi@web.de>
2019-05-04 12:25:55 -0700

    Update tests

--- modules/lazyload-youtube/lazyloadYoutube.test.js
+++ modules/lazyload-youtube/lazyloadYoutube.test.js
@@ -6,21 +6,41 @@ const mockSettings = {
   youtube: {},
 };
 
+function mockVideoUrlInput(override = {}) {
+  const videoUrl = getVideoUrl({
+    videoId: validVideoId,
+    ...override,
+    pluginOptions: {
+      ...defaultPluginOptions,
+      ...override.pluginOptions,
+    },
+  });
+
+  return {
+    url: videoUrl,
+    queryParams: new URLSearchParams(videoUrl),
+  };
+}
+
 // TODO
+// autoplay=1${colour}${controls}${loadpolicy}
+// ${modestbranding}${relations}${playlist}${embedstart}
+
 describe('getVideoUrl', () => {
   it('returns default URL without any overrides', () => {
     // https://www.youtube.com/watch?v=${validVideoId}
-    const videoUrl = getVideoUrl({ pluginOptions: defaultPluginOptions, videoId: validVideoId});
-    expect(videoUrl).toBe(`https://www.youtube.com/embed/${validVideoId}?autoplay=1`);
+    const mockVideo = mockVideoUrlInput();
+    expect(mockVideo.url).toBe(`https://www.youtube.com/embed/${validVideoId}?autoplay=1&rel=0&controls=0&iv_load_policy=3`);
   });
 
   it('supports modestbranding for general override', () => {
-    // https://www.youtube.com/watch?v=${validVideoId}&modestbranding=1
-    const videoUrl = getVideoUrl({ pluginOptions: {
-      ...defaultPluginOptions,
-      modestbranding: true,
-    }, videoId: validVideoId });
-    expect(videoUrl).toBe(`https://www.youtube.com/embed/${validVideoId}?modestbranding=1&autoplay=1`);
+    const mockVideo = mockVideoUrlInput({
+      pluginOptions: {
+        modestbranding: true,
+      }
+    });
+
+    expect(mockVideo.queryParams.get('modestbranding')).toBe('1');
   });
 
   // it('supports modestbranding for link-specific override', () => {
