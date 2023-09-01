defd20f57e0bd076396476fd3f62760311692560
Kevin Weber <kevinweber@users.noreply.github.com>
2023-09-01 16:00:29 +0200

    Test for existence of ajaxStop func before calling it

--- src/frontend/shared-utils/jQueryAjaxStop.js
+++ src/frontend/shared-utils/jQueryAjaxStop.js
@@ -1,9 +1,7 @@
 export default function jQueryAjaxStop(callback) {
   const $ = window.jQuery || window.$;
 
-  if ($) {
-    $(document).ajaxStop(() => {
-      callback();
-    });
+  if ($ && typeof $.ajaxStop === 'function') {
+    $(document).ajaxStop(callback);
   }
 }
